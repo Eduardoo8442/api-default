d42dd862145a2c52b50c5302b2cd0eb5540ca742
Eduardoo <rafael@HP-Pavilion-14-Notebook-PC-42d0c94e.localdomain>
2024-07-17 22:31:03 +0000

    multer installed

---
+++ src/http/upload/image.routes.ts
@@ -0,0 +1,21 @@
+import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+import multerInstance from "../../services/multer/services.multer";
+
+
+
+class ImageRoute {
+constructor(private readonly instance: FastifyInstance) {}
+
+public initConnection(instance: FastifyInstance): void {
+    instance.get('/upload',  { preHandler: multerInstance.single('file') }, async (request: FastifyRequest, reply: FastifyReply) => {
+        if (!request.file) {
+            return reply.status(400).send('Nenhum arquivo foi enviado.');
+          }
+            const filePath = request.file.path;
+            if(filePath) {
+                const imageLink = `${process.env.URL}/uploads/${filePath.replace('uploads/', '')}`;
+                reply.status(200).send({ imageLink });
+            }
+    });
+}
+}
---
+++ src/services/multer/services.multer.ts
@@ -0,0 +1,37 @@
+
+import multer from 'fastify-multer';
+import path from 'path';
+
+class MulterStorage {
+  static getStorage() {
+    return multer.diskStorage({
+      destination: function (req, file, cb) {
+          cb(null, 'uploads/');
+      },
+      filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname);
+        const timestamp = Date.now();
+        cb(null, `${file.fieldname}-${timestamp}${ext}`);
+      }
+  });
+  }
+}
+
+class MulterConfig {
+  static getUpload() {
+    return multer({
+      storage: MulterStorage.getStorage(),
+      fileFilter: function (req, file, cb) {
+          if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+              return cb(new Error('Apenas imagens são permitidas!'));
+          }
+          cb(null, true);
+      },
+      limits: {
+          fileSize: 5 * 1024 * 1024, // 5MB
+      }
+  });
+  }
+}
+const multerInstance = MulterConfig.getUpload();
+export default multerInstance;
